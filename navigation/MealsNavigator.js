a33a86ca68234992fef3f36fadfb183794578212
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-12-30 22:55:25 -0500

    Passing params using navigationOptions

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,7 +1,8 @@
 // we will create a stack of pages
 // on top of the stack is the page on top
 // when we go back it pops the top page and shows the previous page
-
+import { Platform } from 'react-native';
+import Colors from '../constants/Colors';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation'; //always importe from here no matter what version
 
@@ -13,12 +14,29 @@ import MealDetailScreen from '../screens/MealDetailScreen';
 // Then we need to create an AppContainer to put our Navigation container inside the AppContainer
 
 const MealsNavigator = createStackNavigator({   
-    Categories: CategoriesScreen,
+    Categories: { 
+        screen: CategoriesScreen,
+        navigationOptions: {
+            headerTitle: 'Meal Categories'
+        }
+    },
     CategoryMeals: { 
-        screen: CategoryMealScreen
+        screen: CategoryMealScreen,       
     },
     MealDetail: MealDetailScreen 
-});
+}, 
+{   
+    // initialRouteName: 'Categories',
+    mode: 'modal',
+    defaultNavigationOptions: { 
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? Colors.white : Colors.primaryColor,
+    headerTitle: "A Screen"
+}}
+    
+);
 
 export default createAppContainer(MealsNavigator);
 // createStackNavigator/createAppContainer RETURN a React Component, meaning you

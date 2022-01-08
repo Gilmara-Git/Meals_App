089771c0d48e4ea0c5ee67856e710071d4db9e3e
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-01-07 20:56:24 -0500

    feat: updated FavoritesScreen with the new compoment MealList to render MEals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,39 +1,17 @@
 import React from 'react';
-import { 
-    View, 
-    Text, 
-    StyleSheet, 
-    FlatList
-
-} from 'react-native';
-import { MEALS } from '../data/dummy-data'
-// console.log('===================MEALS============')
-// console.log(MEALS)
+import { MEALS } from '../data/dummy-data';
+import MealList from '../components/MealList';
 
 const FavoritesScreen = props =>{
 
     const favMeals = MEALS.filter(meal=> meal.id === 'm1' || meal.id === 'm2');
-    console.log('fav', favMeals )
-
-
-    return (
-        <FlatList
-            data={favMeals}
-            keyExtractor={(item, index)=>item.id}    
-            renderItem={(itemData)=><View><Text>{itemData.item.title}</Text></View>}
-        />
-    );
+    console.log(favMeals, 'favMeasl')
+    
+    return <MealList listData={favMeals} navigation={props.navigation} />
 };
 
 FavoritesScreen.navigationOptions = {
     headerTitle: 'Your Favorites'
 };
 
-const styles =  StyleSheet.create({
-    screen:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-});
 export default FavoritesScreen;
